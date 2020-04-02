af298caf9e49c3990a390a5bff55bf37dbd144ea
maxwelloniate <maxwelloniate@gmail.com>
2020-04-02 14:34:56 -0400

    pageBanner function

--- functions.php
+++ functions.php
@@ -1,5 +1,35 @@
 <?php
 
+function pageBanner($args = NULL)
+{
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+  if (!$args['img']) {
+    if (get_field('page_banner_background_image')) {
+      $args['img'] = get_field('page_banner_background_image')['sizes']['page-banner'];
+    } else {
+      $args['img'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+
+
+?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['img']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php
+}
+
 function university_files()
 {
   wp_enqueue_script('main-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
