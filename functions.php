3ca48c0a2adc16564e23d92d27c968031419afcd
maxwelloniate <maxwelloniate@gmail.com>
2020-04-03 19:40:46 -0400

    Localize script

--- functions.php
+++ functions.php
@@ -37,6 +37,9 @@ function university_files()
   wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_localize_script('main-js', 'universityData', [
+    'root_url' => get_site_url()
+  ]);
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
