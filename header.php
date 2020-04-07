11742fdf5e286fee25f10cda6047f9752b4f3514
maxwelloniate <maxwelloniate@gmail.com>
2020-04-07 18:51:03 -0400

    esc_url

--- header.php
+++ header.php
@@ -11,15 +11,15 @@
 
   <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
-      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <h1 class="school-logo-text float-left"><a href="<?php echo esc_url(site_url()); ?>"><strong>Fictional</strong> University</a></h1>
+      <a href="<?php echo esc_url(site_url('/search')); ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul>
 
             <li <?php if (is_page('about-us') || wp_get_post_parent_id(0) == 12) echo "class='current-menu-item'"; ?>>
-              <a href="<?php echo site_url('/about-us'); ?>">About Us</a>
+              <a href="<?php echo esc_url(site_url('/about-us')); ?>">About Us</a>
             </li>
 
             <li <?php if (get_post_type() == 'program' || is_page('programs')) echo "class='current-menu-item'"; ?>>
@@ -31,7 +31,7 @@
             <li <?php if (get_post_type() == 'campus') echo "class='current-menu-item'"; ?>><a href="<?php echo get_post_type_archive_link('campus') ?>">Campuses</a></li>
 
             <li <?php if (get_post_type() == 'post') echo "class='current-menu-item'"; ?>>
-              <a href="<?php echo site_url('/blog'); ?>">Blog</a>
+              <a href="<?php echo esc_url(site_url('/blog')); ?>">Blog</a>
             </li>
 
           </ul>
@@ -39,7 +39,7 @@
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
           <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
-          <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          <a href="<?php echo esc_url(site_url('/search')); ?>" class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
