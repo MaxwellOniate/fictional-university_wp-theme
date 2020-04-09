6269489a74fee996a67a1adf8329f440dbbc53c6
maxwelloniate <maxwelloniate@gmail.com>
2020-04-09 16:20:28 -0400

    Create new notes

--- page-my-notes.php
+++ page-my-notes.php
@@ -21,6 +21,15 @@ while (have_posts()) {
 ?>
 
   <div class="container container--narrow page-section">
+
+    <div class="create-note">
+      <h2 class="headline headline--medium">Create New Note</h2>
+      <input type="text" placeholder="Title" class="new-note-title">
+      <textarea placeholder="Type in a note here..." class="new-note-body"></textarea>
+      <span class="submit-note">Create Note</span>
+      <span class="note-limit-message">Note limit reached: delete an existing note to make room for a new one.</span>
+    </div>
+
     <ul id="my-notes" class="min-list link-list">
       <?php
       $notes = new WP_Query([
@@ -35,12 +44,12 @@ while (have_posts()) {
 
         <li data-id="<?php the_ID(); ?>">
 
-          <input readonly class="note-title-field" type="text" value="<?php echo esc_attr(get_the_title()); ?>">
+          <input readonly class="note-title-field" type="text" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>">
 
           <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
           <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
 
-          <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+          <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
 
           <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
 
