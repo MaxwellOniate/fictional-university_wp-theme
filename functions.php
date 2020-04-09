92bb2cc6c973cea74855b9abeaa0f0279dad50a6
maxwelloniate <maxwelloniate@gmail.com>
2020-04-09 16:18:38 -0400

    Private notes

--- functions.php
+++ functions.php
@@ -8,6 +8,11 @@ function university_custom_rest()
       return get_the_author();
     }
   ]);
+  register_rest_field('note', 'userNoteCount', [
+    'get_callback' => function () {
+      return count_user_posts(get_current_user_id(), 'note');
+    }
+  ]);
 }
 
 add_action('rest_api_init', 'university_custom_rest');
@@ -143,3 +148,23 @@ function loginCSS()
 }
 
 add_action('login_enqueue_scripts', 'loginCSS');
+
+function makeNotePrivate($data, $postArray)
+{
+  if ($data['post_type'] == 'note') {
+    if (count_user_posts(get_current_user_id(), 'note') > 4 && !$postArray['ID']) {
+      die('You have reached your note limit.');
+    }
+
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
+
+  if ($data['post_type'] == 'note' && $data['post_status'] != 'trash') {
+    $data['post_status'] = "private";
+  }
+
+  return $data;
+}
+
+add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
